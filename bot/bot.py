b5d3adc9ce0688c40430069c88172ffaff1e2554
asaWasa <300401fdsa+->
2021-04-17 21:10:40 +0300

    v3 state machine

--- bot/bot.py
+++ bot/bot.py
@@ -2,6 +2,7 @@ import logging
 from aiogram import types
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher.filters import Text
 from aiogram.utils import executor
 from loader import bot, dp
 from common.constants import USER, INVITE
@@ -19,15 +20,20 @@ class BotState(StatesGroup):
     end = State()  # state ending
 
 
-@dp.message_handler(commands='start')
+def out_keyword_menu():
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
+    markup.add("Сделать пост")
+    markup.add("Настройки", "Выход")
+    return markup
+
+
+@dp.message_handler(commands=['start'], state=None)
 async def cmd_start(message: types.Message,  state: FSMContext):
     user = types.User.get_current()
     if db_user.is_in(USER.ID, user[USER.ID]):
         await BotState.main.set()
         await message.answer("Снова здравствуйте!")
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
-        markup.add("Сделать пост")
-        markup.add("Настройки", "Выход")
+        markup = out_keyword_menu()
         await message.answer("Выберите действие", reply_markup=markup)
     else:
         await message.answer(str(message.from_user.first_name) + " Привет \n Место для красочного приветствия")
@@ -42,17 +48,50 @@ async def process_auth(message: types.Message, state: FSMContext):
         db_user.push(dict(types.User.get_current()))
         await BotState.main.set()
         await message.answer("Успешный вход, добро пожаловать!")
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
-        markup.add("Сделать пост")
-        markup.add("Настройки", "Выход")
+        markup = out_keyword_menu()
         await message.answer("Выберите действие", reply_markup=markup)
     else:
         await message.answer("Такого приглашения не существует, повторите попытку")
 
 
-@dp.message_handler(state=BotState.main)
-async def process_main(message: types.Message, state: FSMContext):
+@dp.message_handler(Text(equals="Сделать пост"), state=BotState.main)
+async def add_post(message: types.Message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
+    markup.add("Назад")
+    await BotState.active.set()
+    await message.answer("Выберите соц сети:", reply_markup=markup)
+    # Вывести все соцсети пользователя и all для отправки во все соц сети
+
+
+@dp.message_handler(Text(equals="Настройки"), state=BotState.main)
+async def get_settings(message: types.Message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
+    markup.add("Назад")
+    await BotState.settings.set()
+    await message.answer("Настройки", reply_markup=markup)
+
+
+@dp.message_handler(Text(equals="Выход"), state=BotState.main)
+async def exit_(message: types.Message, state: FSMContext):
+    markup = types.ReplyKeyboardRemove()
     await state.finish()
+    await message.answer("Вышел", reply_markup=markup)
+
+
+@dp.message_handler(Text(equals="Назад"), state=BotState.active)
+async def go_back(message: types.Message):
+    await BotState.main.set()
+    await message.answer("<-")
+    markup = out_keyword_menu()
+    await message.answer("Выберите действие", reply_markup=markup)
+
+
+@dp.message_handler(Text(equals="Назад"), state=BotState.settings)
+async def go_back(message: types.Message):
+    await BotState.main.set()
+    await message.answer("<-")
+    markup = out_keyword_menu()
+    await message.answer("Выберите действие", reply_markup=markup)
 
 
 if __name__ == '__main__':
