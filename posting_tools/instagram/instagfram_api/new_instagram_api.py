ba2830edba59066f7582458165c3a6989a91c7ea
asaWasa <300401fdsa+->
2021-05-03 01:52:32 +0300

    v4.6

--- posting_tools/instagram/instagfram_api/new_instagram_api.py
+++ posting_tools/instagram/instagfram_api/new_instagram_api.py
@@ -8,6 +8,21 @@ class RequestType:
     GET = 'GET'
 
 
+class NoValidToken(Exception):
+    def __init__(self, text=None):
+        self.text = text
+
+
+class BadImage(Exception):
+    def __init__(self, text=None):
+        self.text = text
+
+
+class BadCaption(Exception):
+    def __init__(self, text=None):
+        self.text = text
+
+
 class InstagramApi:
     def __init__(self, user_object):
         self.params = self.__get_user_param(user_object)
@@ -15,10 +30,9 @@ class InstagramApi:
     def __get_user_param(self, user_object):
         user_data = self.__get_user_data(user_object)
         user_param = dict()
-        # todo заменить инфой из базы
         user_param['access_token'] = user_data['access_token']
         user_param['endpoint_base'] = 'https://graph.facebook.com/v10.0/'
-        user_param['instagram_account_id'] = user_data['instagram_account_id']  # todo заменить инфой из базы
+        user_param['instagram_account_id'] = user_data['instagram_account_id']
         return user_param
 
     def __api_call(self, url, endpoint_data, type):
@@ -37,45 +51,89 @@ class InstagramApi:
 
     def __get_user_data(self, user_object):
         """получить данные пользователя из базы данных и выдать"""
-        #todo поправить
         user_data = dict()
         user_data['access_token'] = user_object['access_token']
         user_data['instagram_account_id'] = user_object['instagram_account_id']
         return user_data
 
     def make_post(self, user_request, delay=5):
-        """создать медиа объект"""
-        media_response = self.__create_media(user_request['image'], user_request['caption'])
-        media_id = media_response['data']['json_data']
-        while media_response['status'] != 'FINISHED':
-            status_media_object = self.get_status_media_object(media_id)
-            media_response['status'] = status_media_object
-            print("---- IMAGE MEDIA OBJECT STATUS -----\n")
-            print("     Status Code:")
-            print("     " + media_response['status'])
-            sleep(delay)
-        """Опубликовать медиа объект"""
-        response_media_object = self.__posting_media(media_id)
-        posting_limit = self.get_limit_posting_content()
-        return response_media_object, posting_limit
+        try:
+            """создать медиа объект"""
+            media_response = self.__create_media(user_request['image'], user_request['caption'])
+            media_id = media_response['data']['json_data']
+            while media_response['status'] != 'FINISHED':
+                status_media_object = self.get_status_media_object(media_id)
+                media_response['status'] = status_media_object
+                print("---- IMAGE MEDIA OBJECT STATUS -----")
+                print("     Status Code:")
+                print("     " + media_response['status'])
+                sleep(delay)
+            """Опубликовать медиа объект"""
+            response_media_object = self.__posting_media(media_id)
+            posting_limit = self.get_limit_posting_content()
+            print("---- POSTING MEDIA OBJECT STATUS -----")
+            print("     ID:")
+            print("     " + str(response_media_object['json_data']))
+            print("     POSTING LIMIT USAGE:")
+            print("     " + str(posting_limit['json_data']))
+            return response_media_object, posting_limit
+
+        except NoValidToken:
+            #todo вернуть запрос в базу
+            pass
+        except BadImage:
+            # todo вернуть запрос в базу
+            pass
+        except BadCaption:
+            # todo вернуть запрос в базу
+            pass
+        except Exception as e:
+            # todo вернуть запрос в базу
+            print('error - {}'.format(e))
+            pass
 
     def __create_media(self, image, caption, type_media='img'):
+        try:
+            url = self.params['endpoint_base'] + self.params['instagram_account_id'] + '/media'
+            endpoint_data = dict()
+            endpoint_data['caption'] = caption
+            endpoint_data['access_token'] = self.params['access_token']
+            if type_media == 'img':
+                endpoint_data['image_url'] = image
+            else:
+                endpoint_data['media_type'] = "VIDEO"
+                endpoint_data['video_url'] = self.params['media_url']
+            response = {'data': self.__api_call(url, endpoint_data, 'POST'),
+                        'status': 'IN_PROGRESS'}
+            self.__check_response_errors(response)
+            return response
+        except NoValidToken:
+            print('---------------ERROR TOKEN---------------')
+            raise NoValidToken
+        except BadImage:
+            print('---------------ERROR IMAGE---------------')
+            raise BadImage
+        except BadCaption:
+            print('---------------ERROR CAPTION---------------')
+            raise BadCaption
+        except Exception as e:
+            print('error - {}'.format(e))
+            raise e
 
-        url = self.params['endpoint_base'] + self.params['instagram_account_id'] + '/media'
-        endpoint_data = dict()
-        endpoint_data['caption'] = caption
-        endpoint_data['access_token'] = self.params['access_token']
-        if type_media == 'img':
-            endpoint_data['image_url'] = image
-        else:
-            endpoint_data['media_type'] = "VIDEO"
-            endpoint_data['video_url'] = self.params['media_url']
-        response = {'data': self.__api_call(url, endpoint_data, 'POST'),
-                    'status': 'IN_PROGRESS'}
-        return response
+    def __check_response_errors(self, data):
+        try:
+            data = data['data']['json_data']['error']
+            subcode = data['error_subcode']
+            if subcode == 463:
+                raise NoValidToken
+        except NoValidToken:
+            raise NoValidToken
+        except Exception as e:
+            raise e
 
     def get_status_media_object(self, created_object_id):
-        url = self.params['endpoint_base'] + '/' + created_object_id
+        _id = created_object_id['id']
+        url = self.params['endpoint_base'] + '/' + _id
         endpoint_data = dict()
         endpoint_data['fields'] = 'status_code'
         endpoint_data['access_token'] = self.params['access_token']
@@ -85,7 +143,7 @@ class InstagramApi:
     def __posting_media(self, created_object_id):
         url = self.params['endpoint_base'] + self.params['instagram_account_id'] + '/media_publish'
         endpoint_data = dict()
-        endpoint_data['creation_id'] = created_object_id
+        endpoint_data['creation_id'] = created_object_id['id']
         endpoint_data['access_token'] = self.params['access_token']
         return self.__api_call(url, endpoint_data, 'POST')
 
@@ -100,5 +158,5 @@ class InstagramApi:
 api = InstagramApi({'instagram_account_id': '17841403220449260',
                     'access_token': 'EAAGpsNw9iWoBAEWZAeMU57Rps7rslZC4Gf0btFUDLdtLtR5kjXOFZC8HVYhuyU4TNW8OtCLrkPyrbMiuT2d9bZCeXtK0lepLaolcT3Ffcx6JbOXVONZCNFyNjsh9R1JLCEwj9Q5bplatFTZCSW6Fk5I905XUyZA9NZB8bCTa9NIqy682qLi6mJO4TYyg2kPqZBwHF0ByOZBxfwTBCi8DQTenZAyk0TlJNZBZCe6L0qCNZABWmR8rN21wiRRretioWgF7VsIEMZD'})
 
-api.make_post({'image': 'https://images-na.ssl-images-amazon.com/images/I/91m9UMB4p5L._SX500_.jpg',
-               'caption': 'v2'})
+api.make_post({'image': 'https://st3.depositphotos.com/7358190/15593/v/600/depositphotos_155934414-stock-illustration-sexy-surprised-blonde-pop-art.jpg',
+               'caption': 'ohh #test_api'})
